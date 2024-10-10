368c8194e55f87b42f241148e3b8b7f13eae2b8d
Long Nguyen <longbeo1605@gmail.com>
2024-10-10 09:28:56 +0700

    ar kit object

---
+++ android/app/src/main/kotlin/com/example/demo_deep_ar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_deep_ar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:math' as math;
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late ARKitController arkitController;
+  ARKitSphere? sphere;
+
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
+  }
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    this.arkitController.add(_placeObject());
+    this.arkitController.onNodeTap = (nodes) => onNodeTapHandler(nodes);
+  }
+
+  ARKitNode _placeObject() {
+    final material =
+    ARKitMaterial(diffuse: ARKitMaterialProperty.color(Colors.yellow));
+    sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+
+    return ARKitNode(
+      name: 'sphere',
+      geometry: sphere,
+      position: vector.Vector3(0, 0, -0.5),
+    );
+  }
+
+  void onNodeTapHandler(List<String> nodesList) {
+    final name = nodesList.first;
+    final color =
+    (sphere!.materials.value!.first.diffuse as ARKitMaterialColor).color ==
+        Colors.yellow
+        ? Colors.blue
+        : Colors.yellow;
+    sphere!.materials.value = [
+      ARKitMaterial(diffuse: ARKitMaterialProperty.color(color))
+    ];
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) =>
+          AlertDialog(content: Text('You tapped on $name')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('ARKit in Flutter'),
+        ),
+        body: ARKitSceneView(
+          enableTapRecognizer: true,
+          onARKitViewCreated: onARKitViewCreated,
+        ),
+      );
+}
